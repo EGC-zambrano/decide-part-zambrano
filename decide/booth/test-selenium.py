b9fd779e04e549742c17915593ac6fc9c84a34ea
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-01 16:32:09 +0000

    test: Homepage selenium test

---
+++ decide/booth/test-selenium.py
@@ -0,0 +1,43 @@
+import time
+
+from base.tests import BaseTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class HomepageTestCase(StaticLiveServerTestCase):
+    def setUp(self):
+        # Crea un usuario admin y otro no admin
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        # Opciones de Chrome
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+
+        self.base.tearDown()
+
+    def test_simpleCorrectLogin(self):
+        # Abre la ruta del navegador
+        self.driver.get(f"{self.live_server_url}/")
+
+        # Verifica que el nombre de la página sea el correcto
+        self.assertTrue(self.driver.title == "Decide | Homepage")
+
+        # Verifica que los elementos de la homepage existen
+        self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "header")) == 1)
+        self.assertTrue(len(self.driver.find_elements(By.TAG_NAME, "footer")) == 1)
+        self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "hero")) == 1)
+        self.assertTrue(len(self.driver.find_elements(By.ID, "hero-text")) == 1)
