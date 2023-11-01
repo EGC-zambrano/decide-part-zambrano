da6b641b999ef5d882eea393d4bc7894aeb26abb
JMGarCas <jmgarciacasillas135@gmail.com>
2023-11-01 16:45:58 +0100

    feat: diseño inical homepage

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,17 +17,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from booth.views import index
 
 
-schema_view = get_swagger_view(title='Decide API')
+schema_view = get_swagger_view(title="Decide API")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('doc/', schema_view),
-    path('gateway/', include('gateway.urls')),
+    path("admin/", admin.site.urls),
+    path("doc/", schema_view),
+    path("gateway/", include("gateway.urls")),
+    path("", include("booth.urls")),  # Booth routes
 ]
 
 for module in settings.MODULES:
-    urlpatterns += [
-        path('{}/'.format(module), include('{}.urls'.format(module)))
-    ]
+    urlpatterns += [path("{}/".format(module), include("{}.urls".format(module)))]
