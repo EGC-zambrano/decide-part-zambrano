6a104c66a37fd1a1cc31ece81cddf5587a19b229
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-01 20:16:00 +0000

    feat: Login form and endpoints

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -1,29 +1,59 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import TemplateView
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.status import (
-        HTTP_201_CREATED,
-        HTTP_400_BAD_REQUEST,
-        HTTP_401_UNAUTHORIZED
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
 )
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import ObjectDoesNotExist
 
+from .forms import LoginForm
 from .serializers import UserSerializer
+
+
+# Non-api view
+class LoginView(TemplateView):
+    def post(self, request):
+        form = LoginForm(request.POST)
+
+        msg = None
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = "Invalid credentials"
+        else:
+            msg = "Error validating the form"
+
+        return render(request, "authentication/login.html", {"form": form, "msg": msg})
+
+    def get(self, request):
+        form = LoginForm(None)
+
+        return render(request, "authentication/login.html", {"form": form, "msg": None})
 
 
 class GetUserView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         tk = get_object_or_404(Token, key=key)
         return Response(UserSerializer(tk.user, many=False).data)
 
 
 class LogoutView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         try:
             tk = Token.objects.get(key=key)
             tk.delete()
@@ -35,13 +65,13 @@ class LogoutView(APIView):
 
 class RegisterView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         tk = get_object_or_404(Token, key=key)
         if not tk.user.is_superuser:
             return Response({}, status=HTTP_401_UNAUTHORIZED)
 
-        username = request.data.get('username', '')
-        pwd = request.data.get('password', '')
+        username = request.data.get("username", "")
+        pwd = request.data.get("password", "")
         if not username or not pwd:
             return Response({}, status=HTTP_400_BAD_REQUEST)
 
@@ -52,4 +82,4 @@ class RegisterView(APIView):
             token, _ = Token.objects.get_or_create(user=user)
         except IntegrityError:
             return Response({}, status=HTTP_400_BAD_REQUEST)
-        return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
+        return Response({"user_pk": user.pk, "token": token.key}, HTTP_201_CREATED)
