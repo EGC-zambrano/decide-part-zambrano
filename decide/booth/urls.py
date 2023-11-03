2dd0e323f1a9a11256164469c917c1111287d9fe
marnunrey2 <marnunrey2@alum.us.es>
2023-11-03 12:30:02 +0100

    feat: Creación nueva página para el registro de usuarios (funciona)

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -5,5 +5,6 @@ from .views import BoothView
 
 urlpatterns = [
     path("", views.index, name="homepage"),
+    path("register/", views.register, name="register"),
     path("<int:voting_id>/", BoothView.as_view()),
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -1,6 +1,9 @@
 import json
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.http import Http404
 
@@ -9,6 +12,42 @@ from base import mods
 
 def index(request):
     return render(request, "booth/homepage.html")
+
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30, required=True, help_text="Required. 30 characters or fewer."
+    )
+    last_name = forms.CharField(
+        max_length=30, required=True, help_text="Required. 30 characters or fewer."
+    )
+    email = forms.EmailField(
+        required=True, help_text="Required. Enter a valid email address."
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        )
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(
+                "login"
+            )  # Redirect to the login page after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "booth/register.html", {"form": form})
 
 
 # TODO: check permissions and census
