f02cbb1ffff412b8ede6a7e4d7e521cae15c84dc
marnunrey2 <marnunrey2@alum.us.es>
2023-11-03 13:25:29 +0100

    style: Estilo de página añadido siguiendo el estilo del homepage

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -15,15 +15,9 @@ def index(request):
 
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(
-        max_length=30, required=True, help_text="Required. 30 characters or fewer."
-    )
-    last_name = forms.CharField(
-        max_length=30, required=True, help_text="Required. 30 characters or fewer."
-    )
-    email = forms.EmailField(
-        required=True, help_text="Required. Enter a valid email address."
-    )
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
