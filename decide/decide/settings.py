9d5b8f407501ababc721060364c8cb8d8891ddd9
Marchabar <marchabar1@alum.us.es>
2023-11-11 19:56:36 +0100

    fix: add variable for the site id

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +33,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-SITE_ID = 2
+SITE_ID = int(os.getenv('DJANGO_SITE_ID', '2'))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
