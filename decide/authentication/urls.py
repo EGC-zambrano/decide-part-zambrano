6e97e24bf0a8d1cf71c2921cf4a3779663a41457
marnunrey2 <marnunrey2@alum.us.es>
2023-11-23 20:19:39 +0100

    feat: Creacion de reseteo de contraseña funcional a falta de que funcione enviar el email

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
-from .views import GetUserView, LoginView, LogoutView, RegisterView, ChangePasswordView
+from .views import (
+    GetUserView,
+    LoginView,
+    LogoutView,
+    RegisterView,
+    ChangePasswordView,
+)
 
 urlpatterns = [
     path("login/", obtain_auth_token),
@@ -11,4 +23,30 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("register/", RegisterView.as_view(), name="register"),
     path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+    path(
+        "restore-password/",
+        PasswordResetView.as_view(template_name="authentication/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "restore-password/done/",
+        PasswordResetDoneView.as_view(
+            template_name="authentication/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="authentication/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(
+            template_name="authentication/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
