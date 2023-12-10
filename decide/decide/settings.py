d86548095f14b6774e118f7bbb80b91c2769929c
auroranavas <navasaurora@outlook.com>
2023-12-10 20:42:05 +0100

    test: debugging

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = True
 
 if DEBUG:
     SECRET_KEY = "^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+"
