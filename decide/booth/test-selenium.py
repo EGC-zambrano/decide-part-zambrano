b639fa474fc651252e8e461017a8f6b703bbaf76
JMGarCas <jmgarciacasillas135@gmail.com>
2023-12-17 00:01:29 +0100

    tests: simple and multiple voting selenium

--- decide/booth/test-selenium.py
+++ decide/booth/test-selenium.py
@@ -14,7 +14,8 @@ from django.utils import timezone
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from authentication.models import EmailCheck
-from voting.models import Question, Voting
+from voting.models import Question, QuestionOption, Voting
+from django.test import Client
 
 
 class HomepageTestCase(StaticLiveServerTestCase):
@@ -317,3 +318,90 @@ class OpinionsViewTestCase(StaticLiveServerTestCase):
 
         # Verifica que hay un boton de enviar
         self.assertTrue(len(self.driver.find_elements(By.ID, "submit-opinion")) == 1)
+
+
+class VotingListViewTestCase(StaticLiveServerTestCase):
+    def setUp(self):
+        self.client = Client()
+
+        # Crea un usuario admin y otro no admin
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        user = User.objects.get(username="noadmin")
+        EmailCheck.objects.create(user=user, emailChecked=True)
+
+        # Crea votacion simple
+        question1 = Question.objects.create(desc="Test question", question_type="S")
+        for i in range(5):
+            opt = QuestionOption(question=question1, option="option {}".format(i + 1))
+            opt.save()
+        voting1 = Voting.objects.create(
+            name="Simple Voting",
+            desc="Test",
+            start_date=timezone.make_aware(datetime.datetime(2023, 10, 11)),
+            question_id=question1.id,
+        )
+        auth = Auth.objects.get_or_create(
+            url=self.live_server_url, defaults={"me": True, "name": "test auth"}
+        )[0]
+        voting1.auths.add(auth)
+        Census.objects.create(voter_id=user.id, voting_id=voting1.id)
+
+        # Crea votacion multiple
+        question2 = Question.objects.create(desc="Test question", question_type="M")
+        for i in range(5):
+            opt = QuestionOption(question=question2, option="option {}".format(i + 1))
+            opt.save()
+        voting1 = Voting.objects.create(
+            name="Multiple Voting",
+            desc="Test",
+            start_date=timezone.make_aware(datetime.datetime(2023, 10, 11)),
+            question_id=question2.id,
+        )
+        auth = Auth.objects.get_or_create(
+            url=self.live_server_url, defaults={"me": True, "name": "test auth"}
+        )[0]
+        voting1.auths.add(auth)
+        Census.objects.create(voter_id=user.id, voting_id=voting1.id)
+
+        app = SocialApp.objects.create(
+            provider="google",
+            name="Google",
+            client_id="test",
+            secret="test",
+        )
+
+        # Add the current site to the SocialApp's sites
+        app.sites.add(Site.objects.get_current())
+
+        # Opciones de Chrome
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(options=options)
+
+        # Disable recaptcha
+        os.environ["DISABLE_RECAPTCHA"] = "1"
+
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+
+        self.base.tearDown()
+
+    def test_simple_voting(self):
+        self.driver.get(f"{self.live_server_url}/signin")
+        self.driver.find_element(By.ID, "id_username").send_keys("noadmin")
+        self.driver.find_element(By.ID, "id_password").send_keys("qwerty")
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+
+        self.driver.get(f"{self.live_server_url}/voting-list")
+
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".question_type")
+
+        self.assertEqual(len(elements), 2)
+        self.assertEqual(elements[0].text, "Single")
+        self.assertEqual(elements[1].text, "Multiple")
