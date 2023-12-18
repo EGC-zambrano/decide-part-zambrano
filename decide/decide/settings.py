0942b5fa182e1900d169cf43d7d046139002a4c4
marnunrey2 <marnunrey2@alum.us.es>
2023-12-18 14:13:40 +0100

    fix: hide email information

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -209,8 +209,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "decidezambrano@gmail.com"
-EMAIL_HOST_PASSWORD = "yhgl cbmn baaj ppzs"
 
 
 try:
