2197f2f872299a0c89bc3d0c07cb1bca29d31e08
MartaInes <MartaInes@users.noreply.github.com>
2023-12-18 15:14:42 +0000

    refactor: Format code with Black

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -191,7 +191,6 @@ class VotingTestCase(BaseTestCase):
         for q in v.postproc:
             self.assertEqual(tally.get(q["number"], 0), q["votes"])
 
-
     def test_create_voting_from_api(self):
         data = {"name": "Example"}
         response = self.client.post("/voting/", data, format="json")
