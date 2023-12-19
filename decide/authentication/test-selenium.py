d1f5b5ea257327d1acada982005bfb6ed6cc1e1c
Aurora Navas <73229219+auroranavas@users.noreply.github.com>
2023-12-19 12:00:59 +0100

    refactor: remove comments

--- decide/authentication/test-selenium.py
+++ decide/authentication/test-selenium.py
@@ -146,12 +146,6 @@ class LoginPageTestCase(StaticLiveServerTestCase):
             "Error en el formulario",
         )
 
-
-"""
-
-"""
-
-
 class LoginGoogleTestCase(StaticLiveServerTestCase):
     def setUp(self):
         self.base = BaseTestCase()
